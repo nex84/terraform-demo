cd85df6cce94a16ca911e512918ab14b1bd8a410
Alexandre Schwartzmann <schwartzmann.a@gmail.com>
2020-02-13 10:33:52 +0100

    terrafrm 0.12 syntax

--- input.tf
+++ input.tf
@@ -19,10 +19,10 @@ provider "azurerm" {
   // version = "=1.22.1"
 //   version = ">=1.22.0"
   
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 terraform {
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "AppRG" {
     name     = "${var.appName}RG"
-    location = "${var.location}"
+    location = var.location
 
     tags {
         environment = "poc"
@@ -11,8 +11,8 @@ resource "azurerm_resource_group" "AppRG" {
 resource "azurerm_virtual_network" "AppVNet" {
     name                = "${var.appName}VNet"
     address_space       = ["10.0.0.0/16"]
-    location            = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.AppRG.name}"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.AppRG.name
 
     tags {
         environment = "poc"
@@ -22,16 +22,16 @@ resource "azurerm_virtual_network" "AppVNet" {
 # Create subnet
 resource "azurerm_subnet" "AppSubnet" {
     name                 = "${var.appName}Subnet"
-    resource_group_name  = "${azurerm_resource_group.AppRG.name}"
-    virtual_network_name = "${azurerm_virtual_network.AppVNet.name}"
+    resource_group_name  = azurerm_resource_group.AppRG.name
+    virtual_network_name = azurerm_virtual_network.AppVNet.name
     address_prefix       = "10.0.1.0/24"
 }
 
 # Create public IPs
 resource "azurerm_public_ip" "AppPublicIP" {
     name                         = "${var.appName}PublicIP"
-    location                     = "${var.location}"
-    resource_group_name          = "${azurerm_resource_group.AppRG.name}"
+    location                     = var.location
+    resource_group_name          = azurerm_resource_group.AppRG.name
     allocation_method = "Dynamic"
 
     tags {
@@ -42,8 +42,8 @@ resource "azurerm_public_ip" "AppPublicIP" {
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "AppNSG" {
     name                = "${var.appName}NSG"
-    location            = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.AppRG.name}"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.AppRG.name
 
     security_rule {
         name                       = "SSH"
@@ -88,17 +88,17 @@ resource "azurerm_network_security_group" "AppNSG" {
 
 # Create network interface
 resource "azurerm_network_interface" "AppNIC" {
-    count                     = "${var.VMCount}"
+    count                     = var.VMCount
     name                      = "${var.appName}NIC-${count.index}"
-    location                  = "${var.location}"
-    resource_group_name       = "${azurerm_resource_group.AppRG.name}"
-    network_security_group_id = "${azurerm_network_security_group.AppNSG.id}"
+    location                  = var.location
+    resource_group_name       = azurerm_resource_group.AppRG.name
+    network_security_group_id = azurerm_network_security_group.AppNSG.id
 
     ip_configuration {
         name                          = "${var.appName}NICConfig"
-        subnet_id                     = "${azurerm_subnet.AppSubnet.id}"
+        subnet_id                     = azurerm_subnet.AppSubnet.id
         private_ip_address_allocation = "dynamic"
-        // public_ip_address_id          = "${azurerm_public_ip.AppPublicIP.id}"
+        // public_ip_address_id          = azurerm_public_ip.AppPublicIP.id
     }
 
     tags {
@@ -109,7 +109,7 @@ resource "azurerm_network_interface" "AppNIC" {
 # Generate random text for a unique storage account name
 resource "random_id" "randomId" {
     keepers = {
-        resource_group = "${azurerm_resource_group.AppRG.name}"
+        resource_group = azurerm_resource_group.AppRG.name
     }
 
     byte_length = 8
@@ -118,8 +118,8 @@ resource "random_id" "randomId" {
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "AppSA" {
     name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = "${azurerm_resource_group.AppRG.name}"
-    location                    = "${var.location}"
+    resource_group_name         = azurerm_resource_group.AppRG.name
+    location                    = var.location
     account_tier                = "Standard"
     account_replication_type    = "LRS"
 
@@ -130,10 +130,10 @@ resource "azurerm_storage_account" "AppSA" {
 
 # Create virtual machine
 resource "azurerm_virtual_machine" "AppVM" {
-    count                 = "${var.VMCount}"
+    count                 = var.VMCount
     name                  = "${var.appName}VM-${count.index}"
-    location              = "${var.location}"
-    resource_group_name   = "${azurerm_resource_group.AppRG.name}"
+    location              = var.location
+    resource_group_name   = azurerm_resource_group.AppRG.name
     network_interface_ids = ["${element(azurerm_network_interface.AppNIC.*.id, count.index)}"]
     vm_size               = "Standard_DS1_v2"
 
@@ -166,11 +166,11 @@ resource "azurerm_virtual_machine" "AppVM" {
 
     boot_diagnostics {
         enabled = "false"
-        storage_uri = "${azurerm_storage_account.AppSA.primary_blob_endpoint}"
+        storage_uri = azurerm_storage_account.AppSA.primary_blob_endpoint
     }
 
     tags {
-        environment = "poc",
-        id = "${count.index}"
+        environment = "poc"
+        id = count.index
     }
 }
